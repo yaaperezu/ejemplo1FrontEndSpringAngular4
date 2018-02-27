0355d4c0a9917e53930c55030d9c5f1081d10552
yaaperezu <yasserperez10@gmail.com>
2018-02-26 20:04:46 -0500

    El proyecto esta funcionando, se adiciona el modulo de rutas y se crea el componente de usuarios

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+    { path: '', redirectTo: 'appComponent', pathMatch: 'full' },
+    { path: 'appComponent', component: AppComponent }
+] 
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { UserComponent } from './user/user.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
